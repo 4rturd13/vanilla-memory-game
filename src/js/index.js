6366ed145cc7b7bf5c795cd2a88549d2be443eed
4rturd13 <4rturd13@gmail.com>
2020-01-19 16:49:27 -0500

    darkmode

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,10 @@
 //Dark mode ==>
+const btnSwitch = document.querySelector("#switch");
+
+btnSwitch.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    btnSwitch.classList.toggle("active");
+});
 
 //Dark mode <==
 
